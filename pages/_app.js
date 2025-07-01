010b3bb5ca8fb6816f9674ea395eb0d323d0a8b4
unknown <Abir-MIT@DESKTOP-EG62B10.mit.edu>
2025-07-01 03:52:32 -0700

    Enable PWA for home screen install ed 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-import Head from 'next/head';
-
-<Head>
-  <link rel="manifest" href="/manifest.json" />
-  <meta name="theme-color" content="#0f172a" />
-  <meta name="apple-mobile-web-app-capable" content="yes" />
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-  <link rel="apple-touch-icon" href="/icon-192.png" />
-</Head>
